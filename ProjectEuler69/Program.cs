9eb01f8f8db9f49c491e40bff5d327813d6355e1
Github User <this@isafakeadress.lose.tld>
2013-06-22 13:20:45 +0100

    Initial commit
    Obvious way of computing totients is slow (gcd(a,b) for large a,b is
    slooooow!)

---
+++ ProjectEuler69/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler69
+{
+    class Program
+    {
+        static int GCD(int a, int b)
+        {
+            if (a == a) return a;
+            else if (a > b) return GCD(a - b, b);
+            else return GCD(a, b - a);
+        }
+
+        static void Main(string[] args)
+        {
+            Dictionary<int, int> Totients = new Dictionary<int, int>();
+
+            const int max = 1000000;
+            int accum;
+            for (int i = 2; i <= max; i++)
+            {
+                accum = 0;
+                for (int j = 1; j < i; j++)
+                {
+                    if (GCD(i, j) == 1) accum++;
+                }
+                Totients.Add(i, accum);
+                if(i%1000==0)Console.WriteLine(i);
+            }
+            foreach (KeyValuePair<int, int> x in Totients)
+                Console.WriteLine(x.Key + " " + x.Value);
+        }
+    }
+}
